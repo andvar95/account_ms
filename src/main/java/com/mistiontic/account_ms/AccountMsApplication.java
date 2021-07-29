5af91433d86072c507824a29c5af1ef39bc22910
andvar95 <varelo_95@hotmail.com>
2021-07-28 20:37:47 -0500

    add person class

--- src/main/java/com/mistiontic/account_ms/AccountMsApplication.java
+++ src/main/java/com/mistiontic/account_ms/AccountMsApplication.java
@@ -1,13 +1,23 @@
 package com.mistiontic.account_ms;
 
+import com.mistiontic.account_ms.models.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class AccountMsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountMsApplication.class, args);
+	}
+
+	@GetMapping
+	public  List<Person> hello(){
+		return List.of(new Person("1","Andres","varelo@gmail",26));
 	}
 
 }
---
+++ src/main/java/com/mistiontic/account_ms/models/Person.java
@@ -0,0 +1,57 @@
+package com.mistiontic.account_ms.models;
+
+public class Person {
+    public String id;
+    public String name;
+    public String email;
+    public Integer age;
+
+    public Person(String id, String name, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
